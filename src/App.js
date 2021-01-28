177724c8365250c3e3ab6551639d829fb8b0c4d5
Pinia <pinyazchko.oleg@gmail.com>
2021-01-28 18:43:27 +0200

    Implementing of PrivateRoute

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-//import { AuthProvider } from "./Auth";
-//import PrivateRoute from "./PrivateRoute";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+
 
 const App = () => {
   return (
-
+      <AuthProvider>
         <Router>
           <div>
-            <Route exact path="/" component={Home} />
+            <PrivateRoute exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
           </div>
         </Router>
-
+      </AuthProvider>
   );
 };
 
