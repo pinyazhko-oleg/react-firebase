1e163efed24003b89661cc542bf3110c119f14d6
Pinia <pinyazchko.oleg@gmail.com>
2021-01-28 19:35:50 +0200

    Implementing of Register & Login logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import app from "../base.js";
+import { AuthContext } from "../Auth.js";
 
-const Login = () => {
+const Login = ({ history }) => {
+    const handleLogin = useCallback(
+        async event => {
+            event.preventDefault();
+            const { email, password } = event.target.elements;
+            try {
+                await app
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
+                history.push("/");
+            } catch (error) {
+                alert(error);
+            }
+        },
+        [history]
+    );
+
+    const { currentUser } = useContext(AuthContext);
+
+    if (currentUser) {
+        return <Redirect to="/" />;
+    }
+
     return (
-        <>
-            <h1>Login</h1>
-        </>
+        <div>
+            <h1>Log in</h1>
+            <form onSubmit={handleLogin}>
+                <label>
+                    Email
+                    <input name="email" type="email" placeholder="Email" />
+                </label>
+                <label>
+                    Password
+                    <input name="password" type="password" placeholder="Password" />
+                </label>
+                <button type="submit">Log in</button>
+            </form>
+        </div>
     );
 };
 
-export default Login;
+export default withRouter(Login);
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { withRouter } from "react-router";
+import app from "../base";
 
-const Register = () => {
+const Register = ({ history }) => {
+    const handleSignUp = useCallback(async event => {
+        event.preventDefault();
+        const { email, password } = event.target.elements;
+        try {
+            await app
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
+            history.push("/");
+        } catch (error) {
+            alert(error);
+        }
+    }, [history]);
+
     return (
-        <>
+        <div>
             <h1>Register</h1>
-        </>
+            <form onSubmit={handleSignUp}>
+                <label>
+                    First name
+                    <input name="displayFirstName" placeholder="First name" />
+                </label>
+                <label>
+                    Last name
+                    <input name="displayLastName" placeholder="Last name" />
+                </label>
+                <label>
+                    Email
+                    <input name="email" type="email" placeholder="Email" />
+                </label>
+                <label>
+                    Password
+                    <input name="password" type="password" placeholder="Password" />
+                </label>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     );
 };
 
-export default Register;
+export default withRouter(Register);
