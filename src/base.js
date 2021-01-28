7bdda01495fa6b4d6c184faa402a86c8d3d5db37
Pinia <pinyazchko.oleg@gmail.com>
2021-01-28 18:15:47 +0200

    Adding Auth, base and .env

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBRoULO_EemEStrJgxOHOGB4_Il3HqfR9A",
+    authDomain: "react-firebase-3c7bb.firebaseapp.com",
+    projectId: "react-firebase-3c7bb",
+    storageBucket: "react-firebase-3c7bb.appspot.com",
+    messagingSenderId: "356204951896",
+    appId: "1:356204951896:web:fc4908fcaf6f15cc59cd2d",
+    measurementId: "G-018T64CHW0"
+});
+
+export default app;
